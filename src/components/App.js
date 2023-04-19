43236e6256bb3bbf84b19e962555c892edafa502
Sang Kibet Elkana <elkanahkibet6@gmail.com>
2023-04-19 18:36:27 +0300

    Added the AboutList and Article  Component

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import blogData from "../data/blog";
 import Header from "./Header";
 import About from "./About";
+import ArticleList from "./ArticleList";
 
 console.log(blogData);
 
@@ -9,11 +10,10 @@ function App() {
 
   return (
     <div className="App">
-      You're on your own from here! Follow the deliverables; test things out in
-      the browser as you write your code; and good luck!
     
       <Header name={blogData.name}/>
       <About image={blogData.image} about={blogData.about}/>
+      <ArticleList posts={blogData.posts}/>
      
 
     </div>
